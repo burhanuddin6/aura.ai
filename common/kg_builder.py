fb9a6b00230a8d474c0a3c1ae0fe7c371c7e85a6
Burhanuddin Aliasghar Ezzi <98301121+burhanuddin6@users.noreply.github.com>
2025-03-24 22:21:25 +0500

    neo4j knowledge graph builder (buggy)

---
+++ common/kg_builder.py
@@ -0,0 +1,57 @@
+import asyncio
+import os
+from neo4j import GraphDatabase
+from neo4j_graphrag.embeddings import OllamaEmbeddings
+from neo4j_graphrag.experimental.pipeline.kg_builder import SimpleKGPipeline
+from neo4j_graphrag.llm import OllamaLLM
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+# Connect to the Neo4j database
+driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
+
+# List the entities and relations the LLM should look for in the text
+entities = ["Person", "House", "Planet"]
+relations = ["PARENT_OF", "HEIR_OF", "RULES"]
+potential_schema = [
+    ("Person", "PARENT_OF", "Person"),
+    ("Person", "HEIR_OF", "House"),
+    ("House", "RULES", "Planet"),
+]
+
+# Create an Embedder object
+embedder = OllamaEmbeddings(model="nomic-embed-text")
+
+# Instantiate the LLM
+llm = OllamaLLM(
+    model_name="deepseek-r1:7b",
+    # model_params={
+    #     "max_tokens": 2000,
+    #     "response_format": {"type": "json_object"},
+    #     "temperature": 0,
+    # },
+)
+
+# Instantiate the SimpleKGPipeline
+kg_builder = SimpleKGPipeline(
+    llm=llm,
+    driver=driver,
+    embedder=embedder,
+    entities=entities,
+    relations=relations,
+    on_error="IGNORE",
+    from_pdf=False,
+)
+
+# Run the pipeline on a piece of text
+text = (
+    "The son of Duke Leto Atreides and the Lady Jessica, Paul is the heir of House "
+    "Atreides, an aristocratic family that rules the planet Caladan."
+)
+asyncio.run(kg_builder.run_async(text=text))
+driver.close()
--- common/llm.py
+++ common/llm.py
@@ -1,13 +1,29 @@
 import os
 from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
+from langchain_ollama import OllamaEmbeddings
 
 # Load environment variables from a .env file
 load_dotenv()
 
 # Get the model name from the environment variable, with a default fallback
 model_name = os.getenv("LLM_MODEL_NAME")
+embedding_model_name = os.getenv("EMBEDDING_MODEL_NAME")
 
 llm = ChatOllama(
     model=model_name,
 )
+
+emb_model = OllamaEmbeddings(
+    model="deepseek-r1:7b",
+)
+r1 = emb_model.embed_documents(
+    [
+        "Alpha is the first letter of Greek alphabet",
+        "Beta is the second letter of Greek alphabet",
+    ]
+)
+r2 = emb_model.embed_query(
+    "What is the second letter of Greek alphabet"
+)
+
