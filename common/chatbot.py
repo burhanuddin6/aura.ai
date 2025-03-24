ca844865af7cd81df6141486d46c9123965e97ea
Burhanuddin Aliasghar Ezzi <98301121+burhanuddin6@users.noreply.github.com>
2025-03-24 15:10:32 +0500

    llm with chat history

--- common/chatbot.py
+++ common/chatbot.py
@@ -1,4 +1,3 @@
-from langchain_openai import ChatOpenAI
 from langchain_core.prompts import (
     ChatPromptTemplate,
     MessagesPlaceholder,
@@ -7,26 +6,42 @@ from langchain_core.prompts import (
 )
 from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
-from langchain_ollama import OllamaLLM
-
-model = OllamaLLM(model="llama3")
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from llm import llm
 
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessagePromptTemplate.from_template(
             "You are a nice chatbot having a conversation with a human."
         ),
-        # The `variable_name` here is what must align with memory
+        # The `variable_name` here is what must align wi(prompt | llm)th memory
         MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{question}")
     ]
 )
-# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
-# Notice that `"chat_history"` aligns with the MessagesPlaceholder name.
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-conversation = LLMChain(
-    llm=model,
-    prompt=prompt,
-    verbose=True,
-    memory=memory
-)
+
+# Initialize chat history
+chat_history = []
+chain = (prompt | llm)
+# Chat loop
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Chatbot: Goodbye!")
+        break
+
+    # Invoke the conversation with the current question and chat history
+    response = chain.invoke(
+        {
+            "question": user_input,
+            "chat_history": chat_history
+        }
+    )
+
+    # Print the chatbot's response
+    print(f"Chatbot: {response}")
+
+    # # Update chat history
+    chat_history.append(HumanMessage(user_input))
+    chat_history.append(AIMessage(response.content))
+
